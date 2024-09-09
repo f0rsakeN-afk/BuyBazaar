7a0cb07125161e29cf9fa1b0102d53365f5908d1
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-09-09 08:24:30 +0545

    added itemtype.ts

---
+++ frontend/src/types/itemTypes.ts
@@ -0,0 +1,15 @@
+export interface itemTypes {
+  name: string;
+  category: string;
+  image: string;
+  shortDescription: string;
+  description: string;
+  price: number;
+  discount?: number;
+  weight: number;
+  CountryOfOrigin: string;
+  quality: string;
+  check: string;
+  minWeight: number;
+  rating: number;
+}
