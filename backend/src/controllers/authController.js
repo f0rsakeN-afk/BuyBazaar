687a09d0e1f6da28e3c81d55c8166258882d9c86
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-09-16 21:40:59 +0545

    added restricted routes updatepw functionality and accessroute

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,6 +2,7 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
@@ -37,5 +38,46 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next("User email or password incorrect", 400);
   }
+  createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) {
+    return next(new AppError("You're not logged in. Please login to get access", 401));
+  }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const user = await User.findById(decoded.id);
+  if (!user) {
+    return next(new AppError("The user belonging to this token does not exist", 400));
+  }
+
+  if (user.passwordChangedAfter(decoded.iat)) {
+    return next(new AppError("The user recently changed the password. Please login again", 401));
+  }
+  req.user = user;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError("You are not authorized to perform this action", 401));
+    }
+    next();
+  };
+};
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError("Your password is wrong", 401));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
   createSendToken(user, 200, res);
 });
