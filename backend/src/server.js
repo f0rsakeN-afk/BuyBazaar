d2f24fca3ee00dd2b6e0e75a5eb8e8320ef85c2b
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-09-16 21:39:11 +0545

    added db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose')
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
 
-dotenv.config({ path: './config.env' })
-
+dotenv.config({ path: path.resolve(__dirname, "config.env") });
 
 const PORT = process.env.PORT;
+const DB = process.env.DB;
+const PW = process.env.PW;
+
+const DBURL = DB.replace("<db_password>", PW);
+
+mongoose
+  .connect(DBURL)
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
