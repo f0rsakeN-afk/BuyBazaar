171c417cf7ed1891f6d0995531ccd2a4d383fb13
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-09-19 21:44:00 +0545

    updated error handling

--- backend/src/controllers/errorController.js
+++ backend/src/controllers/errorController.js
@@ -1,25 +1,39 @@
 const AppError = require("../utils/appError");
 
 const sendErrorDev = (err, res) => {
-  let error = { ...err };
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
 };
 
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
   } else {
+    console.error("ERROR 💥", err);
     res.status(500).json({
-      status: "fail",
+      status: "error",
       message: "Something went very wrong",
     });
   }
 };
 
 module.exports = (err, req, res, next) => {
-  err.status = err.status || "fail";
   err.statusCode = err.statusCode || 500;
-  if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
-  } else if (process.env.NODE_ENV === "development") {
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    error.message = err.message;
+
+    sendErrorProd(error, res);
   }
 };
