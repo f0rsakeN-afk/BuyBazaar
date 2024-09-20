8c75fec9d7a53bdfca36c1fd3d7c5d2d28480b2f
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-09-20 18:31:05 +0545

    added about route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/userRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const aboutRoutes = require("./routes/aboutRoutes");
+const globalErrorHandler = require("./controllers/errorController");
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request to ${req.url}`);
+  next();
+});
+
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/contact", contactRoutes);
+app.use("/api/v1/about", aboutRoutes);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
